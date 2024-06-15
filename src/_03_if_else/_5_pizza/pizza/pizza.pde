fec16cd677e373f88302a2ce87562f352c679844
league <league@imac-29.attlocal.net>
2024-06-15 12:43:44 -0700

    today i made a pizza

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,34 @@
 
 void setup() {
-    
+    size(500, 500);
+    PImage mushroom;
+    mushroom = loadImage("mushroom.png");
+    image(mushroom,250, 250);
+    PImage pepperoni;
+     pepperoni = loadImage("pepperoni.png");
+         image(pepperoni,250, 250);
+          PImage olive;
+     olive = loadImage("olive.png"); 
+      image(olive,250, 250);
+       PImage mushroom2;
+    mushroom = loadImage("mushroom.png");
+    image(mushroom,150, 150);
+    PImage pepperoni2;
+     pepperoni = loadImage("pepperoni.png");
+         image(pepperoni,100, 100);
+          PImage olive2;
+     olive = loadImage("olive.png"); 
+      image(olive,200, 200);
 }
 void draw() {
+  
+    fill(#D6A04E);
+    ellipse(250, 250,  450, 450);
+    fill(255, 0, 0, 200);
+    ellipse(250, 250,  400, 400);
+    fill(#F2EC25);
+    ellipse(250, 250,  370, 370);
+    
+    if (mousePressed && (mouseButton == RIGHT)) {}
     
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,39 +2,27 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
-		//sign out of you git hub next time or I will do worse things
+		
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+			String input = JOptionPane.showInputDialog("Who makes it, has no need of it.\n" + 
+					"Who buys it, has no use for it. \n" + 
+					"Who uses it can neither see nor feel it. \n" + 
+					"What is it?");
+		
+			// 4. If they got the answer right, pop up "correct!" and increase the score by one
+			if("coffin".equalsIgnoreCase(input)) {
+				JOptionPane.showMessageDialog(null, "correct");
+			}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+			else
+				JOptionPane.showMessageDialog(null, "wrong, it was coffin ");
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
