d18c30d5329232456ef72d6d7f854f3ce1d84128
league <mjbboswell@gmail.com>
2024-07-13 12:56:04 -0700

    Today i did some more loops

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -1,9 +1,36 @@
 package _05_for_loops._2_badgers;
 
 public class Badgers {
-public static void main(String[] args) {
-	for(int x= 0;x<12;x++) {
+	public static void main(String[] args) {
+		
+		for (int y = 0; y < 2; y++) {
+	
+			for (int x = 0; x < 12; x++) {
+
+				System.out.print("Badger");
+
+				if (x < 11) {
+					System.out.print(", ");
+				}
+			}
+			System.out.println();
+			for (int x = 0; x < 2; x++) {
+
+				System.out.print("Mushroom");
+				
+				if (x < 1) {
+					System.out.print(", ");
+					
+					
+				}
+			}
+			
+			System.out.println();
+			System.out.println();
+			
+		}
+		System.out.print("A Snake!!!");
+		
 		
 	}
-}
 }
---
+++ src/_05_for_loops/_3_bananas_4eva/bananas_4eva/bananas_4eva.pde
@@ -0,0 +1,6 @@
+size(1200, 1000);
+for (int y = 0; y < 50; y++) {
+for (int x = 0; x < 20; x++) {
+  text("bannana",x*60,15+ y*15);
+}
+}
